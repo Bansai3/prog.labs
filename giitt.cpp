9f6aa59d7aa50e7146d4097d777fe5302b7e0c1c
georgedem975 <georgiidemyanov@yandex.ru>
2021-10-17 18:38:55 +0300

    модифицируем develop

--- giitt.cpp
+++ giitt.cpp
@@ -6,5 +6,11 @@ int main()
 {
     cout << "Hello world!";
     cout << "if you want to add two numbers, enter +";
+    string a;
+    cin >> a;
+    if (a == "+")
+    {
+        cout <<"Enter nambers:";
+    }
 
 }
