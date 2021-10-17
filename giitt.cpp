5f29594c5a6a011ff9b6d4ca25e2fcb61671c4d1
georgedem975 <georgiidemyanov@yandex.ru>
2021-10-17 18:45:21 +0300

    операция +

--- giitt.cpp
+++ giitt.cpp
@@ -8,9 +8,17 @@ int main()
     cout << "if you want to add two numbers, enter +";
     string a;
     cin >> a;
+    int b, c;
     if (a == "+")
     {
         cout <<"Enter nambers:";
+        cout << "Enter first namber:";
+        cin >> b;
+        cout <<"Enter second namber:";
+        cin >> c;
+        cout << "Ответ:";
+        cout << b + c;
+
     }
 
 }
